c8372c6c67a87b49dba8dc0fbf3ce2c572973e32
Maksim <Soldatenko>
2016-11-07 21:20:56 +0200

    Simple.Data completed

--- Micro Orm/MicroOrm/MicroOrm/SimpleDataDemoOPerations.cs
+++ Micro Orm/MicroOrm/MicroOrm/SimpleDataDemoOPerations.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MicroOrm.Domain;
+using Simple.Data;
 
 namespace MicroOrm
 {
@@ -11,42 +13,79 @@ namespace MicroOrm
     {
         public override void ClearOrders()
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            db.Orders.DeleteAll(); // db.Orders.DeleteAll(db.Orders.Id > 7);
         }
 
         public override void FillOrders()
         {
-            throw new NotImplementedException();
+            var orders = new List<Order>()
+            {
+                new Order() { CustomerId = 1, Date = DateTime.Now, Comments = "Inserted by Simple.Data multi-insert."},
+                new Order() { CustomerId = 7, Date = DateTime.Now, Comments = "Inserted by Simple.Data multi-insert."},
+                new Order() { CustomerId = 9, Date = DateTime.Now, Comments = "Inserted by Simple.Data multi-insert."}
+            };
+
+            var db = Database.Open();
+            db.Orders.Insert(orders);
         }
 
         public override List<Order> GetOrders()
         {
-            throw new NotImplementedException();
+            var result = new List<Order>();
+
+            var db = Database.Open();
+            var orders = db.Orders.All();
+
+            foreach (var order in orders)
+            {
+                var ord = (Order) order;
+                result.Add(ord);
+            }
+
+            return orders;
         }
 
         public override Order GetOrder(int id)
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            var order = db.Orders.Find(db.Orders.Id == id); //Gets TOP 1
+            var ord = (Order) order;
+
+            return ord;
         }
 
         public override void UpdateOrder(int orderId, string comment)
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            db.Orders.UpdateById(Id: orderId, Comments: comment); // UpdateAll
         }
 
         public override void DeleteOrder(int id)
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            db.Orders.DeleteById(id); //DeleteAll
         }
 
         public override List<Order> GetOrdersByCustomerId(int customerId)
         {
-            throw new NotImplementedException();
+            var result = new List<Order>();
+
+            var db = Database.Open();
+            var orders = db.Orders.FindAllByCustomerId(customerId);
+            foreach (var order in orders)
+            {
+                var ord = (Order) order;
+                result.Add(ord);
+            }
+
+            return result;
         }
 
         public override void InsertOrder(Order order)
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            db.Orders.Insert(order);
         }
     }
 }
