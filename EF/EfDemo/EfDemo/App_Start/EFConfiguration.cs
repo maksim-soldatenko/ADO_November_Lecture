7bb3a33308ab2e861a197de99717236100c67601
Maksim <Soldatenko>
2016-11-14 21:21:51 +0200

    EfDemo Transient faults

--- EF/EfDemo/EfDemo/App_Start/EFConfiguration.cs
+++ EF/EfDemo/EfDemo/App_Start/EFConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
 using System.Web;
 using EfDemo.CommandInterception;
+using EfDemo.DbConfigurations;
 
 namespace EfDemo.App_Start
 {
@@ -12,6 +14,7 @@ namespace EfDemo.App_Start
         public static void Configure()
         {
             DbInterception.Add(new DemoInterceptor());
+            DbConfiguration.SetConfiguration(new DemoConfiguration());
         }
     }
 }
---
+++ EF/EfDemo/EfDemo/DbConfigurations/DemoConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+
+namespace EfDemo.DbConfigurations
+{
+    public class DemoConfiguration: DbConfiguration
+    {
+        public DemoConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new DemoExecutionStrategy(5, TimeSpan.FromSeconds(5)));
+        }
+    }
+
+    public class DemoExecutionStrategy: DbExecutionStrategy
+    {
+        public DemoExecutionStrategy(int maxRetryCount, TimeSpan maxDelay):base(maxRetryCount, maxDelay)
+        {
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            var logger = new Logger.Logger();
+
+            logger.Warning(exception, "ALARM{0}", Environment.NewLine);
+
+            return true;
+        }
+    }
+}
